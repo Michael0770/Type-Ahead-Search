e7e2350e43a80ffc1f42ab996ac214ad0966bbf8
michael0770 <hyman0770@gmail.com>
2024-08-06 17:48:28 +1000

    Update Related files

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import React from 'react';
+import './App.css';
+import TypeAheadSearch from './TypeAheadSearch';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="mb-4">Type Ahead Search</h1>
+        <TypeAheadSearch />
+      </header>
+    </div>
+  );
+}
+
+export default App;
